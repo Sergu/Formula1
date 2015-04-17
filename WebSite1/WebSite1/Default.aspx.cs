a0e645d5e73690bce073559a9a0084a013e978f3
Sergu <sergushov.1995@mail.ru>
2015-04-17 13:29:12 +0300

    continuation Formula 1 Site
    some style changes, tables add

---
+++ WebSite1/WebSite1/Default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    //public List<DriverInfoPilot>;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            if (DriversStandings.GetDriverStandings() == null)
+            {
+                DriversStandings.MakeDriversStandings();
+            }
+            RepeaterDrivers.DataSource = DriversStandings.GetDriverStandings();
+            RepeaterDrivers.DataBind();
+        }
+    }
+    protected void DriverInfoButton_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("~/Standings/DriverInfo.aspx");
+    }
+    protected void CurrentRaceButton_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("~/Standings/RaceResultsCurrent.aspx");
+    }
+}
